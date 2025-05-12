d9136e216d7a9a59d12a530695defe039d065ee8
Shitakichi <easyfish0429@gmail.com>
2025-05-12 17:48:20 +0900

    Add files via upload

---
+++ C/lesson6/exam1.c
@@ -0,0 +1,21 @@
+/*
+ * 学生の成績を構造体で管理する。
+ * int型のid, japanese, math, science, social_studies, english, sumとdouble型のaverageを構造体にし、新しいデータ型としてstudent_tとする。
+ * student_tを5個の配列を作り、以下の値で初期化する。
+ * {{1, 80, 80, 80, 80, 80}, {2, 90, 85, 80, 85, 100}, {3, 70, 88, 90, 90, 68}, {4, 85, 88, 90, 98, 98}, {5, 80, 82, 84, 84, 90}, }
+ * for文を使い、sumにjapanese, math, science, social_studies, englishの合計を代入し、averageに平均を代入しなさい。
+ * for文を使い、構造体の中身を表示させなさい。
+ *
+ * [出力例]
+ * ID: 1, 合計: 400, 平均: 80.00
+ * ID: 2, 合計: 440, 平均: 88.00
+ * ID: 3, 合計: 406, 平均: 81.20
+ * ID: 4, 合計: 459, 平均: 91.80
+ * ID: 5, 合計: 420, 平均: 84.00
+ */
+#include <stdio.h>
+
+int main(void){
+    return 0;
+}
+ 
